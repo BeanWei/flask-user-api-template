d14e327c582f43991845dc1dce8c69b1593b61cb
BeanWei <157318439@qq.com>
2018-04-22 02:16:06 +0800

    屏蔽配置

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ class BaseConfig:
     MAIL_USE_SSL = True           # 重要，qq邮箱需要使用SSL
     MAIL_USE_TLS = False          # 不需要使用TLS
     MAIL_USERNAME = '157318439@qq.com'  # 填邮箱
-    MAIL_PASSWORD = 'xttvnyuzklhqbjeg'      # 填授权码
+    MAIL_PASSWORD = 'ttvnyuzklhqbjeg'      # 填授权码
     FLASK_MAIL_SENDER = '皮皮虾！我们走！<13114469013@qq.com>'   #邮件发送方
     FLASK_MAIL_SUBJECT_PREFIX = '[皮皮虾！我们走]'     #邮件标题
     #MAIL_DEFAULT_SENDER = 'xxx@qq.com'  # 填邮箱，默认发送者
